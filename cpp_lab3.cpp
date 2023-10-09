b1628f018472f87c2e4098e0d79753fe6a7004c7
IvanIv99 <finrez205@gmail.com>
2023-10-09 22:51:25 +0300

    Create cpp_lab3.cpp

---
+++ cpp_lab3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int arr[32];
+int mini = 9999999999999999999;
+
+void Difference(int i, int sum_m, int sum_p, int n) {
+    if (i == n) {
+        int d = abs(sum_m - sum_p);
+        if (d < mini) {
+            mini = d;
+        }
+        return;
+    }
+    Difference(i + 1, sum_m + arr[i], sum_p, n);
+    Difference(i + 1, sum_m, sum_p + arr[i], n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    Difference(0, 0, 0, n);
+    cout << mini;
+    return 0;
+}
